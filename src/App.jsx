90e2d4766faf75a4dffa0d25c37798c755be45e4
Marvin M. Pacis <marvedcodes@gmail.com>
2022-12-29 16:16:24 +0800

    Update dependency array to avoid infinite rerender

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
     });
 
     setFilteredMonsters(newFilteredMonsters);
-  }, [monsters, filteredMonsters]);
+  }, [monsters, searchField]);
 
   const onSearchChange = e => {
     const searchFieldString = e.target.value.toLowerCase();
